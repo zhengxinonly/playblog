072cdd51fb6c041a1790f8208c7815008a0c0c31
pyxxp <pyxxponly@gmail.com>
2020-03-30 19:27:51 +0800

    2.2 创建数据库模型

---
+++ modes.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from extensions import db
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+    blog_title = db.Column(db.String(60))
+    blog_sub_title = db.Column(db.String(100))
+    name = db.Column(db.String(30))
+    about = db.Column(db.Text)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    email = db.Column(db.String(254))
+    site = db.Column(db.String(255))
+    body = db.Column(db.Text)
+    from_admin = db.Column(db.Boolean, default=False)
+    reviewed = db.Column(db.Boolean, default=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
