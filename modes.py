b21d029a8e7d5521f6a4c5f0ca2cd073a5ba46b2
pyxxp <pyxxponly@gmail.com>
2020-03-30 19:51:39 +0800

    2.3 数据表关系

--- modes.py
+++ modes.py
@@ -17,12 +17,20 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True)
 
+    posts = db.relationship('Post', back_populates='category')
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+    auth_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', back_populates='posts')
+
+    comments = db.relationship('Comment', backref='post', cascade='all')
 
 
 class Comment(db.Model):
@@ -34,3 +42,16 @@ class Comment(db.Model):
     from_admin = db.Column(db.Boolean, default=False)
     reviewed = db.Column(db.Boolean, default=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post = db.relationship('Post', back_populates='comments')
+
+    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
+    replied = db.relationship('Comment', back_populates='replies', remote_side=[id])
+    replies = db.relationship('Comment', back_populates='replied', cascade='all, delete-orphan')
+
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    url = db.Column(db.String(255))
