63fad582367b3af61361530054d626845acb4c21
pyxxp <pyxxponly@gmail.com>
2020-04-02 16:02:56 +0800

    4.1 表单

---
+++ forms.py
@@ -0,0 +1,55 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField, SubmitField, SelectField, TextAreaField, ValidationError,
+                     BooleanField, PasswordField)
+from wtforms.validators import DataRequired, Email, Length, Optional, URL
+
+from models import Category
+
+
+class LoginForm(FlaskForm):
+    """登录表单"""
+    username = StringField('用户名：', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('密码：', validators=[DataRequired(), Length(1, 128)])
+    remember = BooleanField('记住我')
+    submit = SubmitField('登录')
+
+
+class RegisterForm(FlaskForm):
+    """注册表单"""
+    username = StringField('用户名：', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('密码：', validators=[DataRequired(), Length(1, 128)])
+    email = Email('邮箱：')
+    submit = SubmitField('注册')
+
+
+class PostForm(FlaskForm):
+    """文章表单"""
+    title = StringField('标题', [DataRequired(), Length(max=255)])
+    body = TextAreaField('内容')
+    categories = SelectField('文章种类', choices=[], coerce=int)
+    submit = SubmitField("提交")
+
+    # 保证数据与数据库同步
+    def __init__(self):
+        super(PostForm, self).__init__()
+        self.categories.choices = [(c.id, c.name) for c in Category.query.order_by('id')]
+
+
+class CategoryForm(FlaskForm):
+    """分类表单"""
+    name = StringField('类别名', validators=[DataRequired(), Length(1, 30)])
+    submit = SubmitField('确认')
+
+    @staticmethod
+    def validate_name(field):
+        if Category.query.filter_by(name=field.data).first():
+            raise ValidationError('此分类早就存在')
+
+
+class CommentForm(FlaskForm):
+    """评论表单"""
+    author = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 254)])
+    site = StringField('Site', validators=[Optional(), URL(), Length(0, 255)])
+    body = TextAreaField('Comment', validators=[DataRequired()])
+    submit = SubmitField()
